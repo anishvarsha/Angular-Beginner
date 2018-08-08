f421445b82ab2f8b84984fc2c64c110e4081ef30
a0i005r <anish.iska@samsclub.com>
2018-08-08 13:51:11 -0500

    Updated the Model to Use the Lombok plugin

--- src/main/java/com/spring/angular/springmongoangular/model/Customer.java
+++ src/main/java/com/spring/angular/springmongoangular/model/Customer.java
@@ -1,8 +1,12 @@
 package com.spring.angular.springmongoangular.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter @Setter @NoArgsConstructor
 @Document(collection = "customer")
 public class Customer {
     @Id
@@ -12,44 +16,9 @@ public class Customer {
     private int age;
     private boolean active;
 
-    public Customer() {
-    }
 
     public Customer(String name, int age) {
         this.name = name;
         this.age = age;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer [id=" + id + ", name=" + name + ", age=" + age + ", active=" + active + "]";
     }
 }
